b444f870d9cfcb27e25a4258d07fcd05bb45b7cd
Aditya Raj <aditya.rajb@gmail.com>
2021-08-10 13:40:38 +0530

    finish making vedantu frontend

--- pages/vedantu.js
+++ pages/vedantu.js
@@ -1,11 +1,41 @@
-import { Container, Heading, Button, Input, HStack } from "@chakra-ui/react"
+import { Container, Heading, Button, Input, HStack, Text, Link } from "@chakra-ui/react"
 import { useState } from "react"
+import axios from "axios"
 
 export default function vedantu() {
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const urlSubmit = (e) => {
+  const [formError, setFormError] = useState('')
+  const [foundUrls, setFoundUrls] = useState([])
+
+  const urlSubmit = async (e) => {
     e.preventDefault()
     setIsSubmitting(true)
+
+    const url = e.target.url.value;
+
+    if (url.match(/^http(s?)\:\/\/www\.vedantu\.com(.*?)$/) === null) {
+      setIsSubmitting(false)
+      setFormError('Entered URL does not begin with https://www.vedantu.com')
+      return null
+    }
+    
+    const res = await axios.post('/api/vedantu', { url })
+    if (res.data.err) {
+      setIsSubmitting(false)
+      setFormError('An unknown error occoured. Please try again later or with another url')
+      return null
+    }
+
+    if (res.data.pdfs === []) {
+      setIsSubmitting(false)
+      setFormError('No PDFs found in this page')
+      return null
+    }
+
+    setFormError('')
+    setIsSubmitting(false)
+    setFoundUrls(res.data.pdfs)
+
   }
   
   return (
@@ -19,9 +49,26 @@ export default function vedantu() {
         </Heading>
       
         <HStack as="form" onSubmit={e => urlSubmit(e)} mt="6">
-          <Input disabled={isSubmitting}/>
+          <Input disabled={isSubmitting} name="url" type="url" isRequired/>
           <Button variant="solid" colorScheme="orange" type="submit" isLoading={isSubmitting}>Submit</Button>
         </HStack>
+
+        { formError !== '' && <Text color="red.500" mt="2" fontWeight="bold">Error: {formError}</Text> }
+
+        { foundUrls.length !== 0 && 
+          <>
+            <Heading as="h3" size="xl" mt="6">Found {foundUrls.length} PDFs in this page</Heading>
+            <Heading as="h4" size="sm" mt="1" mb="4">Click on them below to view and download them</Heading>
+          </> 
+        }
+
+        {
+          foundUrls.map(url => (
+            <Text mb="2">
+              <Link href={url} isExternal color="orange.500">{url.split('/')[url.split('/').length - 1]}</Link>
+            </Text>
+          ))
+        }
 
       </Container>
     </>
